243c7fc2187457068d1a3da78e39df4104d0cadb
Medokins <nikodemszwast@gmail.com>
2022-09-29 20:02:19 +0200

    add step into scrapping messages from facebook

--- facebook/facebook/spiders/getData.py
+++ facebook/facebook/spiders/getData.py
@@ -1,15 +1,13 @@
 import scrapy
-from scrapy.loader import ItemLoader
 from scrapy.http import FormRequest
 from scrapy import Selector
 import pandas as pd
-
 
 class FacebookSpider(scrapy.Spider):
     """
     Parse FB pages (needs credentials)
     """
-    name = "get_messages"
+    name = "getData"
 
     def __init__(self, email='', password='',  **kwargs):
         super(FacebookSpider, self).__init__(**kwargs)
@@ -26,7 +24,7 @@ class FacebookSpider(scrapy.Spider):
             self.email = email
             self.password = password
 
-        self.start_urls = ['https://mbasic.facebook.com']
+        self.start_urls = ["https://facebook.com/messages"]
 
     def parse(self, response):
         return FormRequest.from_response(
@@ -37,9 +35,7 @@ class FacebookSpider(scrapy.Spider):
         )
 
     def parse_home(self, response):
-
-        res = response.url
-        href = 'https://mbasic.facebook.com/messages/?ref_component=mbasic_home_header&ref_page=%2Fwap%2Fprofile_timeline.php&refid=17'
+        href = "https://facebook.com/messages"
         return scrapy.Request(
             url=href,
             callback=self.parse_page,
@@ -47,38 +43,17 @@ class FacebookSpider(scrapy.Spider):
 
     def parse_page(self, response):
         res = response.body
-        peoples = Selector(text=res).xpath('//h3/a/text()').extract()
+        people = Selector(text=res).xpath('//h3/a/text()').extract()
         people_link = Selector(text=res).xpath('//h3/a/@href').extract()
-        print('Choose the conversation')
-        print(peoples,people_link)
-        for i in range(len(peoples)):
-            print(i,peoples[i])
-        convo = int(input())
+        print("People:\n\n", people)
+        print("people_links:\n\n", people_link)
+        # print('Choose the conversation')
+        # print(peoples, people_link)
+        # for i in range(len(peoples)):
+        #     print(i,peoples[i])
+        # convo = int(input())
 
-        yield scrapy.Request(url='https://mbasic.facebook.com'+ people_link[convo], callback=self.parse_convo)
+        # yield scrapy.Request(url="https://facebook.com/messages"+ people_link[convo], callback=self.parse_convo)
 
     def parse_convo(self,response):
-        res =response.body
-        old_txt = Selector(text=res).xpath('//div[@id="see_older"]/a/@href').extract()
-        txt = Selector(text=res).xpath('//div[@id="messageGroup"]/div/div').extract()
-        dict1={'Name':[],'Text':[],'Time':[]}
-        for a in txt:
-            content = Selector(text=a).xpath('//div/div/a/strong/text()').extract()
-            contentt = Selector(text=a).xpath('//div/div/div/span/text()').extract()
-            time =  Selector(text=a).xpath('//div/div/abbr/text()').extract()
-            try:
-                dict1['Name'].append(content[0])
-                dict1['Text'].append(' '.join(contentt))
-                dict1['Time'].append(time[0])
-            except:
-                pass
-        self.Name.insert(0,dict1['Name'])
-        self.Text.insert(0,dict1['Text'])
-        self.Time.insert(0,dict1['Time'])
-        # df = pd.DataFrame({'Name':})
-        # print(sum(self.Name,[]),sum(self.Time,[]),sum(self.Text,[]))
-        df = pd.DataFrame({'Name':sum(self.Name,[]),'Text':sum(self.Text,[]),'Time':sum(self.Time,[])})
-        df.to_csv('fb_text.csv')
-        print(df)
-
-        yield scrapy.Request(url='https://mbasic.facebook.com'+ old_txt[0], callback=self.parse_convo)
+        yield

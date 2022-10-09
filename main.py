8d08a1aca32d506fd10a27c3365c4905907cef8d
Medokins <nikodemszwast@gmail.com>
2022-10-09 17:48:40 +0200

    add words preparations

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from voiceToText import record_data, convert_to_text
+#from voiceToText import record_data, convert_to_text
+from nltk import word_tokenize
+from nltk.stem.lancaster import LancasterStemmer
+import json
 
 if __name__ == "__main__":
-    record_data(5)
-    print(convert_to_text())
+    with open("intents.json") as file:
+        data = json.load(file)
+    
+    words = []
+    labels = []
+    docs = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = word_tokenize(pattern)
+            words.extend(wrds)
+            docs.append(pattern)
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"]) 

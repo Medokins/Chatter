ec3559457ee7b49336835e5bab77c5f1f819b485
Medokins <nikodemszwast@gmail.com>
2022-10-14 10:53:49 +0200

    add data formating in load_all_messages function

--- processData.py
+++ processData.py
@@ -4,20 +4,24 @@ import json
 import os
 from googletrans import Translator
 
-def load_all_messages(path):
-    file = open(os.path.join(path, "message_1.json"))
-    data = json.load(file, object_hook=parse_obj)
-    df = pd.json_normalize(data['messages'])
+def load_all_messages():
+    df = pd.DataFrame()
 
-    if len(os.listdir("messages")) > 1:
-        for i in np.arange(2, len(os.listdir("messages")) + 1) : 
-            file = open(os.path.join(path, f"message_{i}.json"), encoding='utf8')
-            data = json.load(file, object_hook=parse_obj)
-            df_temp = pd.json_normalize(data['messages'])
-            df = df.append(df_temp)
-        return df
-    else:
-        return df
+    for i in np.arange(1, len(os.listdir("messages")) + 1) : 
+        file = open(os.path.join("messages", f"message_{i}.json"), encoding='utf8')
+        data = json.load(file, object_hook=parse_obj)
+        df_temp = pd.json_normalize(data["messages"])
+        df = df.append(df_temp)
+
+    findal_df = df[["timestamp_ms", "sender_name", "content"]]
+    names = findal_df["sender_name"].unique()
+    dict_map = {names[0] : 0, names[1] : 1}
+    findal_df = findal_df.rename(columns={"sender_name": "sender_id"})
+    findal_df["sender_id"] = findal_df["sender_id"].map(dict_map)
+    findal_df = findal_df.set_index(keys="timestamp_ms", drop=True)
+    findal_df = findal_df.sort_index()
+
+    return findal_df
     
 def parse_obj(obj):
     for key in obj:
@@ -33,4 +37,4 @@ def translate_data(data, lang):
     translation = translator.translate(data, src=lang)
     print(f"{data} ({lang}) --> {translation.text} ({translation.dest})")
 
-print(load_all_messages("messages"))
+print(load_all_messages())
